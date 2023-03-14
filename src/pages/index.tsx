5972cc2354ff82d3df810d2b8f72b6902383daed
iakah <iakah2313@gmail.com>
2023-03-14 12:18:05 +0300

    basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,13 @@ import Head from "next/head";
 export default function Home (){
 
   return (
-    <>
-      <h1>Hello Ian, this is awesome!</h1>
-    </>
+    <div className="h-screen w-screen flex flex-col justify-center items-center relative ">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="border rounded p-8 mt-2 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200"/>
+          <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"/>
+      </div>
+    </div>
   );
 };
-
