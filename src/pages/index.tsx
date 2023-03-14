a5f6b38305c865fb4758e2046b5e102525f7fbf0
root <root@Lancelot.localdomain>
2023-03-14 11:24:45 +0300

    Created repository and added dependencies

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+
+export default function Home (){
+
+  return (
+    <>
+      <h1>Hello Ian, this is awesome!</h1>
+    </>
+  );
+};
+
